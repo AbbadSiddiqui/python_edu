be9a8147758d4be42f01e642d76e7e7630a2c2aa
AbbadSiddiqui <abbad.siddiqui06@gmail.com>
2025-03-23 07:18:03 +0400

    update

--- Demo.py
+++ Demo.py
@@ -1,13 +1,38 @@
 #https://youtu.be/ix9cRaBkVe0?si=qM6tJ77dC-k0F1OI
 
+#WHILE LOOPS#
 #name = input("Enter your name : ")
 #while name == "":
 #    print("You did not enter your name")
 #    name = input("Enter your name : ")
 #print(f"Hello {name}")
 
-num = int(input("Enter a # between 1-10 : "))
-while num < 1 or num > 10 :
-    print(f"{num} is invalid")
-    num = int(input("Enter a # between 1-10 : "))
-print(f"Your number is {num}")
+#num = int(input("Enter a # between 1-10 : "))
+#while num < 1 or num > 10 :
+#    print(f"{num} is invalid")
+#    num = int(input("Enter a # between 1-10 : "))
+#print(f"Your number is {num}")
+
+#FOR LOOPS#
+#NESTED LOOPS#
+
+#SHOPPING CART EXERCISE#
+#foods = []
+#prices = []
+#total = 0
+
+#while True:
+#    food = input("Enter a food to buy (q to quit) : ")
+#    if food.lower() == "q":
+#        break
+#    else:
+#        price = float(input(f"Enter the price of a {food}: AED"))
+#        foods.append(food)
+#        prices.append(price)
+#
+#print("***YOUR CART***")
+#for food in foods:
+#    print(food)
+#for price in prices:
+#    total += price
+#print(f"Your total is {total} AED")
